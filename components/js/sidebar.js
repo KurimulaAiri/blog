2ede4072b6c72360130edcdf5fd14c31e3810c8a
KurimulaAiri <132827937+KurimulaAiri@users.noreply.github.com>
2025-05-28 12:00:27 +0000

    Deploying to gh-pages from @ KurimulaAiri/blog@d3b973712d3291863c9631ce86008038dd215373 🚀

---
+++ components/js/sidebar.js
@@ -0,0 +1,15 @@
+const scrollBox = document.getElementById('sidebar');
+let hideTimeout = null;
+
+function showScrollbar() {
+    scrollBox.classList.remove('hide-scrollbar');
+    if (hideTimeout) clearTimeout(hideTimeout);
+    hideTimeout = setTimeout(() => {
+        scrollBox.classList.add('hide-scrollbar');
+    }, 2000); // 停止滚动 2 秒后隐藏滚动条
+}
+
+scrollBox.addEventListener('scroll', showScrollbar);
+
+// 初始时隐藏滚动条
+scrollBox.classList.add('hide-scrollbar');  
