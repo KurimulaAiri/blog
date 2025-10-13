817a36cf00be0364e38e6971f19682036a86594f
KurimulaAiri <1095370199@qq.com>
2025-10-13 17:25:39 +0800

    修订

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -4,311 +4,314 @@ import { setupTransparentNavbar } from "vuepress-theme-hope/presets/transparentN
 import { enNavbar, zhNavbar } from "./navbar/index.js";
 import { enSidebar, zhSidebar } from "./sidebar/index.js";
 
-export default hopeTheme({
-  hostname: "https://md.s1r0ko.top",
+export default hopeTheme(
+	{
+		hostname: "https://md.s1r0ko.top",
 
-  author: {
-    name: "KurimulaAiri",
-    url: "https://github.com/KurimulaAiri",
-  },
-  logo: "logo.png",
-  favicon: "favicon.ico",
-  fullscreen: true,
-  repo: "KurimulaAiri/blog",
-  darkmode: "toggle",
-  docsDir: "src",
-  docsBranch: "master",
-  editLink: false,
+		author: {
+			name: "KurimulaAiri",
+			url: "https://github.com/KurimulaAiri",
+		},
+		logo: "logo.png",
+		favicon: "favicon.ico",
+		fullscreen: true,
+		repo: "KurimulaAiri/blog",
+		darkmode: "toggle",
+		docsDir: "src",
+		docsBranch: "master",
+		editLink: false,
 
-  blog: {
-    medias: {
-      // Baidu: "https://example.com",
-      // BiliBili: "https://example.com",
-      // Bitbucket: "https://example.com",
-      // Dingding: "https://example.com",
-      // Discord: "https://example.com",
-      // Dribbble: "https://example.com",
-      // Email: "mailto:info@example.com",
-      // Evernote: "https://example.com",
-      // Facebook: "https://example.com",
-      // Flipboard: "https://example.com",
-      // Gitee: "https://example.com",
-      // GitHub: "https://example.com",
-      // Gitlab: "https://example.com",
-      // Gmail: "mailto:info@example.com",
-      // Instagram: "https://example.com",
-      // Lark: "https://example.com",
-      // Lines: "https://example.com",
-      // Linkedin: "https://example.com",
-      // Pinterest: "https://example.com",
-      // Pocket: "https://example.com",
-      // QQ: "https://example.com",
-      // Qzone: "https://example.com",
-      // Reddit: "https://example.com",
-      // Rss: "https://example.com",
-      // Steam: "https://example.com",
-      // Twitter: "https://example.com",
-      // Wechat: "https://example.com",
-      // Weibo: "https://example.com",
-      // Whatsapp: "https://example.com",
-      // Youtube: "https://example.com",
-      // Zhihu: "https://example.com",
-      // VuePressThemeHope: {
-      //   icon: "https://theme-hope-assets.vuejs.press/logo.svg",
-      //   link: "https://theme-hope.vuejs.press",
-      // },
-    },
-  },
+		blog: {
+			medias: {
+				// Baidu: "https://example.com",
+				// BiliBili: "https://example.com",
+				// Bitbucket: "https://example.com",
+				// Dingding: "https://example.com",
+				// Discord: "https://example.com",
+				// Dribbble: "https://example.com",
+				// Email: "mailto:info@example.com",
+				// Evernote: "https://example.com",
+				// Facebook: "https://example.com",
+				// Flipboard: "https://example.com",
+				// Gitee: "https://example.com",
+				// GitHub: "https://example.com",
+				// Gitlab: "https://example.com",
+				// Gmail: "mailto:info@example.com",
+				// Instagram: "https://example.com",
+				// Lark: "https://example.com",
+				// Lines: "https://example.com",
+				// Linkedin: "https://example.com",
+				// Pinterest: "https://example.com",
+				// Pocket: "https://example.com",
+				// QQ: "https://example.com",
+				// Qzone: "https://example.com",
+				// Reddit: "https://example.com",
+				// Rss: "https://example.com",
+				// Steam: "https://example.com",
+				// Twitter: "https://example.com",
+				// Wechat: "https://example.com",
+				// Weibo: "https://example.com",
+				// Whatsapp: "https://example.com",
+				// Youtube: "https://example.com",
+				// Zhihu: "https://example.com",
+				// VuePressThemeHope: {
+				//   icon: "https://theme-hope-assets.vuejs.press/logo.svg",
+				//   link: "https://theme-hope.vuejs.press",
+				// },
+			},
+		},
 
-  locales: {
-    "/en/": {
-      // navbar
-      navbar: enNavbar,
+		locales: {
+			"/en/": {
+				// navbar
+				navbar: enNavbar,
 
-      // sidebar
-      sidebar: enSidebar,
+				// sidebar
+				sidebar: enSidebar,
 
-      footer: "Default footer",
+				footer: "Default footer",
 
-      displayFooter: true,
+				displayFooter: true,
 
-      blog: {
-        description: "A FrontEnd programmer",
-        intro: "/en/intro.html",
-      },
+				blog: {
+					description: "A FrontEnd programmer",
+					intro: "/en/intro.html",
+				},
 
-      metaLocales: {
-        editLink: "Edit this page on GitHub",
-      },
-    },
+				metaLocales: {
+					editLink: "Edit this page on GitHub",
+				},
+			},
 
-    /**
-     * Chinese locale config
-     */
-    "/": {
-      // navbar
-      navbar: zhNavbar,
+			/**
+			 * Chinese locale config
+			 */
+			"/": {
+				// navbar
+				navbar: zhNavbar,
 
-      // sidebar
-      sidebar: zhSidebar,
+				// sidebar
+				sidebar: zhSidebar,
 
-      footer: "<a href='//beian.miit.gov.cn'>闽ICP备2025092620号-1</a> | <a href='//icp.gov.moe/?keyword=20250596' target='_blank'>萌ICP备20250596号</a>",
+				footer:
+					"<a href='//beian.miit.gov.cn'>闽ICP备2025092620号-1</a> | <a href='//icp.gov.moe/?keyword=20250596' target='_blank'>萌ICP备20250596号</a>",
 
-      displayFooter: true,
+				displayFooter: true,
 
-      blog: {
-        description: "咸鱼",
-        intro: "/intro.html",
-      },
+				blog: {
+					description: "咸鱼",
+					intro: "/intro.html",
+				},
 
-      // page meta
-      metaLocales: {
-        editLink: "在 GitHub 上编辑此页",
-      },
-    },
-  },
+				// page meta
+				metaLocales: {
+					editLink: "在 GitHub 上编辑此页",
+				},
+			},
+		},
 
-  encrypt: {
-    config: {
-      "/en/demo/encrypt.html": {
-        hint: "Password: 1234",
-        password: "1234",
-      },
-      "/demo/encrypt.html": {
-        hint: "Password: 1234",
-        password: "1234",
-      },
-    },
-  },
+		encrypt: {
+			config: {
+				"/en/demo/encrypt.html": {
+					hint: "Password: 1234",
+					password: "1234",
+				},
+				"/demo/encrypt.html": {
+					hint: "Password: 1234",
+					password: "1234",
+				},
+			},
+		},
 
-  // enable it to preview all changes in time
-  // hotReload: true,
+		// enable it to preview all changes in time
+		// hotReload: true,
 
-  // These features are enabled for demo, only preserve features you need here
-  markdown: {
-    align: true,
-    alert: true,
-    attrs: true,
-    codeTabs: true,
-    component: true,
-    demo: true,
-    figure: true,
-    footnote: true, // 脚注
-    gfm: true,
-    hint: true,
-    imgLazyload: true,
-    imgSize: true,
-    include: true,
-    mark: true,
-    markmap: true,
-    plantuml: true,
-    spoiler: true,
-    stylize: [
-      {
-        matcher: "Recommended",
-        replacer: ({ tag }) => {
-          if (tag === "em")
-            return {
-              tag: "Badge",
-              attrs: { type: "tip" },
-              content: "Recommended",
-            };
-        },
-      },
-    ],
-    sub: true,
-    sup: true,
-    tabs: true,
-    tasklist: true,
-    vPre: true,
+		// These features are enabled for demo, only preserve features you need here
+		markdown: {
+			align: true,
+			alert: true,
+			attrs: true,
+			codeTabs: true,
+			component: true,
+			demo: true,
+			figure: true,
+			footnote: true, // 脚注
+			gfm: true,
+			hint: true,
+			imgLazyload: true,
+			imgSize: true,
+			include: true,
+			mark: true,
+			markmap: true,
+			plantuml: true,
+			spoiler: true,
+			stylize: [
+				{
+					matcher: "Recommended",
+					replacer: ({ tag }) => {
+						if (tag === "em")
+							return {
+								tag: "Badge",
+								attrs: { type: "tip" },
+								content: "Recommended",
+							};
+					},
+				},
+			],
+			sub: true,
+			sup: true,
+			tabs: true,
+			tasklist: true,
+			vPre: true,
 
-    highlighter: { // 代码行内高亮
-      type: "shiki",
-      notationDiff: true,
-      collapsedLines: true,
-      notationHighlight: true,
-      notationErrorLevel: "true",
-    },
+			highlighter: {
+				// 代码行内高亮
+				type: "shiki",
+				notationDiff: true,
+				collapsedLines: true,
+				notationHighlight: true,
+				notationErrorLevel: "true",
+			},
 
-    // uncomment these if you need TeX support
-    math: {
-      // install katex before enabling it
-      type: "katex",
-      // or install mathjax-full before enabling it
-      // type: "mathjax",
-    },
+			// uncomment these if you need TeX support
+			math: {
+				// install katex before enabling it
+				type: "katex",
+				// or install mathjax-full before enabling it
+				// type: "mathjax",
+			},
 
-    // install chart.js before enabling it
-    // chartjs: true,
+			// install chart.js before enabling it
+			// chartjs: true,
 
-    // install echarts before enabling it
-    // echarts: true,
+			// install echarts before enabling it
+			// echarts: true,
 
-    // install flowchart.ts before enabling it
-    // flowchart: true,
+			// install flowchart.ts before enabling it
+			// flowchart: true,
 
-    // install mermaid before enabling it
-    // mermaid: true,
+			// install mermaid before enabling it
+			// mermaid: true,
 
-    // playground: {
-    //   presets: ["ts", "vue"],
-    // },
+			// playground: {
+			//   presets: ["ts", "vue"],
+			// },
 
-    // install @vue/repl before enabling it
-    // vuePlayground: true,
+			// install @vue/repl before enabling it
+			// vuePlayground: true,
 
-    // install sandpack-vue3 before enabling it
-    // sandpack: true,
+			// install sandpack-vue3 before enabling it
+			// sandpack: true,
 
-    // install @vuepress/plugin-revealjs and uncomment these if you need slides
-    // revealjs: {
-    //   plugins: ["highlight", "math", "search", "notes", "zoom"],
-    // },
-  },
+			// install @vuepress/plugin-revealjs and uncomment these if you need slides
+			// revealjs: {
+			//   plugins: ["highlight", "math", "search", "notes", "zoom"],
+			// },
+		},
 
-  plugins: {
-    blog: true,
+		plugins: {
+			blog: true,
 
-    // Install @waline/client before enabling it
-    // Note: This is for testing ONLY!
-    // You MUST generate and use your own comment service in production.
-    comment: {
-      provider: "Giscus",
-      repo: "KurimulaAiri/blog",
-      repoId: "R_kgDOOYYhOw",
-      category: "Announcements",
-      categoryId: "DIC_kwDOOYYhO84CtH8g"
-    },
+			// Install @waline/client before enabling it
+			// Note: This is for testing ONLY!
+			// You MUST generate and use your own comment service in production.
+			comment: {
+				provider: "Giscus",
+				repo: "KurimulaAiri/blog",
+				repoId: "R_kgDOOYYhOw",
+				category: "Announcements",
+				categoryId: "DIC_kwDOOYYhO84CtH8g",
+			},
 
-    components: {
-      components: ["Badge", "VPCard", "VidStack"],
-    },
+			components: {
+				components: ["Badge", "VPCard", "VidStack"],
+			},
 
-    docsearch: {
-      appId: "ULF54MS9H5",
-      apiKey: "b1179e69fff348d7b0ff1928975af8fd",
-      indexName: "md-s1r0ko",
-      // container: "### REPLACE ME WITH A CONTAINER (e.g. div) ###",
-    },
+			docsearch: {
+				appId: "ULF54MS9H5",
+				apiKey: "b1179e69fff348d7b0ff1928975af8fd",
+				indexName: "md-s1r0ko",
+				// container: "### REPLACE ME WITH A CONTAINER (e.g. div) ###",
+			},
 
-    icon: {
-      assets: [
-        // "fontawesome",
-        "https://cdn.bootcdn.net/ajax/libs/font-awesome/6.7.2/js/all.min.js",
-        "https://cdn.bootcdn.net/ajax/libs/font-awesome/6.7.2/css/all.min.css",
-        "https://cdn.bootcdn.net/ajax/libs/font-awesome/6.7.2/js/regular.min.js",
-        "https://cdn.bootcdn.net/ajax/libs/font-awesome/6.7.2/css/regular.min.css",
-      ],
-      prefix: "fa-solid fa-"
-    },
+			icon: {
+				assets: [
+					// "fontawesome",
+					"https://cdn.bootcdn.net/ajax/libs/font-awesome/6.7.2/js/all.min.js",
+					"https://cdn.bootcdn.net/ajax/libs/font-awesome/6.7.2/css/all.min.css",
+					"https://cdn.bootcdn.net/ajax/libs/font-awesome/6.7.2/js/regular.min.js",
+					"https://cdn.bootcdn.net/ajax/libs/font-awesome/6.7.2/css/regular.min.css",
+				],
+				prefix: "fa-solid fa-",
+			},
 
-    seo: {
-      autoDescription: true,
-    },
+			seo: {
+				autoDescription: true,
+			},
 
-    sitemap: {
-      changefreq: "daily",
-    }
+			sitemap: {
+				changefreq: "daily",
+			},
 
-    // install @vuepress/plugin-pwa and uncomment these if you want a PWA
-    // pwa: {
-    //   favicon: "/favicon.ico",
-    //   cacheHTML: true,
-    //   cacheImage: true,
-    //   appendBase: true,
-    //   apple: {
-    //     icon: "/assets/icon/apple-icon-152.png",
-    //     statusBarColor: "black",
-    //   },
-    //   msTile: {
-    //     image: "/assets/icon/ms-icon-144.png",
-    //     color: "#ffffff",
-    //   },
-    //   manifest: {
-    //     icons: [
-    //       {
-    //         src: "/assets/icon/chrome-mask-512.png",
-    //         sizes: "512x512",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-mask-192.png",
-    //         sizes: "192x192",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-192.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //       },
-    //     ],
-    //     shortcuts: [
-    //       {
-    //         name: "Demo",
-    //         short_name: "Demo",
-    //         url: "/demo/",
-    //         icons: [
-    //           {
-    //             src: "/assets/icon/guide-maskable.png",
-    //             sizes: "192x192",
-    //             purpose: "maskable",
-    //             type: "image/png",
-    //           },
-    //         ],
-    //       },
-    //     ],
-    //   },
-    // },
-  },
-},
-{
-  custom: true,
-}
+			// install @vuepress/plugin-pwa and uncomment these if you want a PWA
+			// pwa: {
+			//   favicon: "/favicon.ico",
+			//   cacheHTML: true,
+			//   cacheImage: true,
+			//   appendBase: true,
+			//   apple: {
+			//     icon: "/assets/icon/apple-icon-152.png",
+			//     statusBarColor: "black",
+			//   },
+			//   msTile: {
+			//     image: "/assets/icon/ms-icon-144.png",
+			//     color: "#ffffff",
+			//   },
+			//   manifest: {
+			//     icons: [
+			//       {
+			//         src: "/assets/icon/chrome-mask-512.png",
+			//         sizes: "512x512",
+			//         purpose: "maskable",
+			//         type: "image/png",
+			//       },
+			//       {
+			//         src: "/assets/icon/chrome-mask-192.png",
+			//         sizes: "192x192",
+			//         purpose: "maskable",
+			//         type: "image/png",
+			//       },
+			//       {
+			//         src: "/assets/icon/chrome-512.png",
+			//         sizes: "512x512",
+			//         type: "image/png",
+			//       },
+			//       {
+			//         src: "/assets/icon/chrome-192.png",
+			//         sizes: "192x192",
+			//         type: "image/png",
+			//       },
+			//     ],
+			//     shortcuts: [
+			//       {
+			//         name: "Demo",
+			//         short_name: "Demo",
+			//         url: "/demo/",
+			//         icons: [
+			//           {
+			//             src: "/assets/icon/guide-maskable.png",
+			//             sizes: "192x192",
+			//             purpose: "maskable",
+			//             type: "image/png",
+			//           },
+			//         ],
+			//       },
+			//     ],
+			//   },
+			// },
+		},
+	},
+	{
+		custom: true,
+	}
 );
