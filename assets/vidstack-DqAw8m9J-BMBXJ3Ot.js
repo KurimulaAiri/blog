315ebc88cd3c4f14bdd20bb7771158e9ddca4d91
KurimulaAiri <132827937+KurimulaAiri@users.noreply.github.com>
2025-11-12 13:48:23 +0000

    Deploying to gh-pages from @ KurimulaAiri/blog@12e5ce75b151724d92855af011ce5a559751a756 🚀

---
+++ assets/vidstack-DqAw8m9J-BMBXJ3Ot.js
@@ -0,0 +1,1 @@
+import{a_ as i,ay as s}from"./app-fxeXAu2X.js";class r{#i;#t;constructor(t){this.#t=t}start(){i(this.#i)&&this.#s()}stop(){s(this.#i)&&window.cancelAnimationFrame(this.#i),this.#i=void 0}#s(){this.#i=window.requestAnimationFrame(()=>{i(this.#i)||(this.#t(),this.#s())})}}export{r as R};
